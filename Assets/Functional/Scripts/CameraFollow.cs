2ed7cbc97d35f8ee3350eef42de48a92c706a342
Nicolas.dev <nicolasrhila@gmail.com>
2023-05-10 15:27:02 -0500

    feat! camara follow add in main camera

--- Assets/Functional/Scripts/CameraFollow.cs
+++ Assets/Functional/Scripts/CameraFollow.cs
@@ -4,52 +4,22 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    //public Transform target; // Este es el objeto que seguirá la cámara
-    //public float distance = 10f; // Esta es la distancia fija entre la cámara y el objetivo
-    //
-    //private void LateUpdate()
-    //{
-    //// Obtiene la posición global del personaje
-    //Vector3 targetPosition = target.TransformPoint(new Vector3(0, 0, -distance));
-    //
-    //// Desplaza la posición de la cámara hacia la posición global del personaje
-    //transform.position = targetPosition;
-    //
-    //// Asegura que la cámara siempre mire hacia el personaje
-    //transform.LookAt(target);
-
-    //}
-
-    public Transform target; // Este es el objeto que seguirá la cámara
-    public float distance = 10f; // Esta es la distancia fija entre la cámara y el objetivo
-    public float height = 3f; // Esta es la altura fija de la cámara sobre el objetivo
-    public float smoothTime = 0.3f; // Este es el tiempo que tardará la cámara en moverse hacia el objetivo
-    public float shakeIntensity = 0.05f; // Intensidad de la vibración de la cámara
+    public Transform target; // Objeto a seguir
+    public float distance = 10f; // Distancia entre la cámara y el objeto a seguir
+    public float smoothSpeed = 0.5f; // Velocidad de movimiento de la cámara
 
     private Vector3 velocity = Vector3.zero;
-    private Vector3 originalPosition;
 
-    private void Start()
+    void LateUpdate()
     {
-        originalPosition = transform.localPosition; // Almacena la posición original de la cámara
+        // Posición actual de la cámara
+        Vector3 currentPosition = transform.position;
+
+        // Posición a la que debe moverse la cámara
+        Vector3 targetPosition = target.position - transform.forward * distance;
+
+        // Movimiento suave de la cámara
+        transform.position = Vector3.SmoothDamp(currentPosition, targetPosition, ref velocity, smoothSpeed);
     }
 
-    private void LateUpdate()
-    {
-        // Obtiene la posición global del personaje
-        Vector3 targetPosition = target.TransformPoint(new Vector3(0, height, -distance));
-
-        // Desplaza la posición de la cámara hacia la posición global del personaje
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
-
-        // Crea una vibración aleatoria de la cámara
-        Vector3 shake = Random.insideUnitSphere * shakeIntensity;
-        shake.z = 0f;
-
-        // Aplica la vibración a la posición de la cámara
-        transform.localPosition = originalPosition + shake;
-
-        // Asegura que la cámara siempre mire hacia el personaje
-        transform.LookAt(target);
-    }
 }
