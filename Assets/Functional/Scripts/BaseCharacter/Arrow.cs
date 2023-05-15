9e836438e50425000655f61f144e68e93d7a6b67
‘chrisgrm’ <chrisgrm@hotmail.com>
2023-05-15 18:07:02 -0500

    inicio de mecanica de flechas

--- Assets/Functional/Scripts/BaseCharacter/Arrow.cs
+++ Assets/Functional/Scripts/BaseCharacter/Arrow.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Arrow : MonoBehaviour
 {
-    // Start is called before the first frame update
+    float bulletSpeed = 10;
+    private Transform frontFocalPoint;
+    Vector3 bulletDirection;
     void Start()
     {
-        
+        frontFocalPoint = GameObject.Find("FocalPoint").transform;
+        bulletDirection = frontFocalPoint.forward;
+        //Destroy(gameObject, 5);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(bulletDirection * bulletSpeed * Time.deltaTime, Space.World);
     }
 }
