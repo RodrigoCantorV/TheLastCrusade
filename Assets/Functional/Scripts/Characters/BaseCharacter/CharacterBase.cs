ce9b8c6794deb6d2c7d0b877fde6a7d4e96321b2
Nicolas.dev <nicolasrhila@gmail.com>
2023-05-22 03:41:59 -0500

    merge with life

--- Assets/Functional/Scripts/Characters/BaseCharacter/CharacterBase.cs
+++ Assets/Functional/Scripts/Characters/BaseCharacter/CharacterBase.cs
@@ -1,11 +1,17 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
 public class CharacterBase : MonoBehaviour
 {
     [Header("Controls")]
     public float playerSpeed;
     public float dashSpeed;
     public float life;
+
+    public Image lifeBar;
+    public GameObject lifeeBar;
+    public float maxLife;
 
     [Header("Animation Smoothing")]
     [Range(0, 1)]
@@ -41,6 +47,12 @@ public class CharacterBase : MonoBehaviour
     public bool estaAfuera;
     public bool estaVivo;
 
+    void Awake() 
+    {
+        maxLife = 160;
+        lifeeBar = GameObject.Find("LifeBar");
+        lifeBar = lifeeBar.GetComponent<Image>();
+    }
 
     // Start is called before the first frame update
     protected virtual void Start()
@@ -90,6 +102,7 @@ public class CharacterBase : MonoBehaviour
     {
         life -= damageAmount;
         animator.SetBool("damage", true);
+        LifeManagement();
         if (life <= 0)
         {
             Die();
@@ -100,6 +113,13 @@ public class CharacterBase : MonoBehaviour
             }
         }
         //animator.SetTrigger("move");
+    }
+
+    public void LifeManagement()
+    {
+        //lifeBar.GetComponent<Image>().fillAmount = life / maxLife;
+        lifeBar.fillAmount = life / maxLife;
+        Debug.Log("resta vidaa");
     }
 
     void Die()
