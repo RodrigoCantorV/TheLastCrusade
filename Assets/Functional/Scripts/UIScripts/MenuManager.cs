3fcdb396121a1f63a07947c20b0869898391af38
Nicolas.dev <nicolasrhila@gmail.com>
2023-05-22 01:05:18 -0500

    new menu initial

--- Assets/Functional/Scripts/UIScripts/MenuManager.cs
+++ Assets/Functional/Scripts/UIScripts/MenuManager.cs
@@ -1,34 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    [SerializeField] public GameObject menuInit;
-    [SerializeField] public GameObject menuSelect;
+    //[SerializeField] public GameObject menuInit;
+    //[SerializeField] public GameObject menuSelect;
 
-    void Start() 
+   /*  void Start() 
     {
+        SceneManager.LoadScene("SelectChar");
         menuInit.SetActive(true);
         menuSelect.SetActive(false);
-    }
+    } */
 
     public void StartButton()
     {
-        menuInit.SetActive(false);
-        menuSelect.SetActive(true);
+        SceneManager.LoadScene("SelectChar");
+        /* menuInit.SetActive(false);
+        menuSelect.SetActive(true); */
     }
 
     public void MenuSelectCharacter()
     {
-        menuInit.SetActive(false);
-        menuSelect.SetActive(true);
+        SceneManager.LoadScene("SelectChar");
+        /* menuInit.SetActive(false);
+        menuSelect.SetActive(true); */
     }
 
     public void ReturnBack()
     {
-        menuInit.SetActive(true);
-        menuSelect.SetActive(false);
+        SceneManager.LoadScene("MenuInicial");
+        /* menuInit.SetActive(true);
+        menuSelect.SetActive(false); */
     }
 
 
