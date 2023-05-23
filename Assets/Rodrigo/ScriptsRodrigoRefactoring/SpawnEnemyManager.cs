6c53111f58a9b8aea993e9cf5c0f95a52831cef6
RodrigoCantorV <rcvunicun@gmail.com>
2023-05-23 00:37:07 -0500

    Correccion de remataredea

--- Assets/Rodrigo/ScriptsRodrigoRefactoring/SpawnEnemyManager.cs
+++ Assets/Rodrigo/ScriptsRodrigoRefactoring/SpawnEnemyManager.cs
@@ -28,6 +28,12 @@ public class SpawnEnemyManager : MonoBehaviour
             int positionCube = Random.Range(0, positions.Count);
             GameObject enemy = EnemyPool.Instance.RequestEnemy("EnemyBase1");
             enemy.transform.position = positions[positionCube].transform.position;
+            CapsuleCollider collider = enemy.gameObject.GetComponent<CapsuleCollider>();
+            if (collider != null)
+            {
+                // Desactiva el CapsuleCollider
+                collider.enabled = true;
+            }
         }
 
         if ((amountWave % waveBaseEnemy2) == 0)
@@ -38,6 +44,12 @@ public class SpawnEnemyManager : MonoBehaviour
                 int positionCube = Random.Range(0, positions.Count);
                 GameObject enemy = EnemyPool.Instance.RequestEnemy("EnemyBase2");
                 enemy.transform.position = positions[positionCube].transform.position;
+                CapsuleCollider collider = enemy.gameObject.GetComponent<CapsuleCollider>();
+                if (collider != null)
+                {
+                    // Desactiva el CapsuleCollider
+                    collider.enabled = true;
+                }
             }
         }
 
@@ -50,6 +62,12 @@ public class SpawnEnemyManager : MonoBehaviour
                 int positionCube = Random.Range(0, positions.Count);
                 GameObject enemy = EnemyPool.Instance.RequestEnemy("EnemyBoss1");
                 enemy.transform.position = positions[positionCube].transform.position;
+                CapsuleCollider collider = enemy.gameObject.GetComponent<CapsuleCollider>();
+                if (collider != null)
+                {
+                    // Desactiva el CapsuleCollider
+                    collider.enabled = true;
+                }
             }
         }
     }
