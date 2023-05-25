3be9eac24969318552648ad947e9cd6518fb3174
RodrigoCantorV <rcvunicun@gmail.com>
2023-05-25 10:25:53 -0500

    Creditos

--- Assets/Rodrigo/ScriptsRodrigoRefactoring/CreditosManager.cs
+++ Assets/Rodrigo/ScriptsRodrigoRefactoring/CreditosManager.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class CreditosManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject[] Cinematics;
+    private new Animator animation;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        StartCoroutine(NextCinematic());
+    }
+    IEnumerator NextCinematic()
+    {
+        yield return new WaitForSeconds(1f);
+        animation = Cinematics[0].GetComponent<Animator>();
+        animation.SetBool("isActive", true);
+
+        yield return new WaitForSeconds(7.8f);
+        animation = Cinematics[1].GetComponent<Animator>();
+        animation.SetBool("isActive", true);
+
+        yield return new WaitForSeconds(7.8f);
+        animation = Cinematics[2].GetComponent<Animator>();
+        animation.SetBool("isActive", true);
+
+        yield return new WaitForSeconds(7.8f);
+        animation = Cinematics[3].GetComponent<Animator>();
+        animation.SetBool("isActive", true);
+
+        yield return new WaitForSeconds(7.8f);
+        animation = Cinematics[4].GetComponent<Animator>();
+        animation.SetBool("isActive", true);
+
+        yield return new WaitForSeconds(7.8f);
+        animation = Cinematics[5].GetComponent<Animator>();
+        animation.SetBool("isActive", true);
+
+        yield return new WaitForSeconds(7.8f);
+        animation = Cinematics[6].GetComponent<Animator>();
+        animation.SetBool("isActive", true);
     }
 }
+
